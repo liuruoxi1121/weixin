79ed1af8e2ab8b333323a8ef61488c6191b8c5d5
liuruoxi <1104069280@qq.com>
2017-11-14 22:25:33 +0800

    数据框架

--- pages/chazhao/index.js
+++ pages/chazhao/index.js
@@ -30,4 +30,29 @@ Page({
         })
        console.log(chazhao.detail.value) 
     },
+    
+    onLoad(){
+        console.log('页面加载')
+    },
+    onShow(){
+        console.log('页面显示')
+    },
+    onReady(){
+        console.log('页面首次渲染完成')
+    },
+    onHide(){
+        console.log('页面隐藏')
+    },
+    onUnload(){
+        console.log('页面卸载')
+    },
+    onPullDownRefresh(){
+        console.log('页面下拉刷新')
+    },
+    onReachBottom(){
+        console.log('页面到底了')
+    },
+    onPageScroll(calculations){
+        console.log(calculations)
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,37 @@
 const app=getApp()
 Page({
     data:{
-        slides:null
+       greeting:''
     },
-    onload(){
+    greet(greeting){
+        return greeting[Math.floor(Math.random()*greeting.length)]
+    },
+    onLoad(){
+        console.log('页面加载')
         this.setData({
-            slides:app.globalData.slides
+            greeting:this.greet(app.globalData.greeting)
         })
+    },
+    onShow(){
+        console.log('页面显示')
+    },
+    onReady(){
+        console.log('页面首次渲染完成')
+    },
+    onHide(){
+        console.log('页面隐藏')
+    },
+    onUnload(){
+        console.log('页面卸载')
+    },
+    onPullDownRefresh(){
+        console.log('页面下拉刷新')
+    },
+    onReachBottom(){
+        console.log('页面到底了')
+    },
+    onPageScroll(calculations){
+        console.log(calculations)
     }
+   
 })
