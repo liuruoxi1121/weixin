bb9575ddc369b8c536667213d1f56d18485b2b26
liuruoxi <1104069280@qq.com>
2017-11-14 21:18:15 +0800

    剧集页文本框、按钮

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 App({
-    onShow(){
+  onLaunch(options){
+      console.log('初始化')
+      console.log(options)
+  },
+    onShow(options){
         console.log('显示')
+        console.log(options)
+    },
+    onHide(){
+        console.log('隐藏')
     },
     onError(error){
-    console.log(error)
-}
+        console.log(error)
+    }
 })
