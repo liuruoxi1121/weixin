8515d72abcd818037145c7996dd86d0d989500ff
liuruoxi <1104069280@qq.com>
2017-11-16 14:03:03 +0800

    界面美化，样式修改，文本框编辑

--- pages/chazhao/liebiao.js
+++ pages/chazhao/liebiao.js
@@ -1,5 +1,14 @@
 const app=getApp()
 
 Page({
+    formSubmit(chazhao){
+        console.log(chazhao.detail.value)
+    },
+    inputHandler(chazhao){
+        this.setData({
+            pick:chazhao.detail.value
+        })
+       console.log(chazhao.detail.value) 
+    },
    
 })
